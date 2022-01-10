a0df4096013b9b8d21ccae22f8387f16485d8825
Zendai <victorray91@pm.me>
2022-01-11 00:00:28 +0100

    Fixed launch command

--- launch.sh
+++ launch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # @ZendaiOwl
+PACKAGES="git cron curl wget rsync apt-transport-https g++ make"
 CONTAINER="build-script_build_1"
-sudo docker exec -it "$CONTAINER" sh -c docker/scripts/gcc-build/build-musl.sh
+sudo docker exec -it "$CONTAINER" bash \
+apt-get install -y --no-install-recommends "$PACKAGES" \
+scripts/gcc-build/build-musl.sh
 
 exit
