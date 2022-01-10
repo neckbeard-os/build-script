27532eb5bb54432a22e466d6231e421c02e6fd29
Zendai <victorray91@pm.me>
2022-01-11 00:10:15 +0100

    Hmm

--- launch.sh
+++ launch.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # @ZendaiOwl
-PACKAGES="git cron curl wget rsync apt-transport-https g++ make"
 CONTAINER="build-script_build_1"
-sudo docker exec -it "$CONTAINER" bash \
-apt-get install -y --no-install-recommends "$PACKAGES" \
-scripts/gcc-build/build-musl.sh
+sudo docker exec -it "$CONTAINER" bash -c 'apt-get install -y --no-install-recommends git cron curl wget rsync apt-transport-https g++ make
+scripts/gcc-build/build-musl.sh'
 
 exit
--- scripts/gcc-build/build-musl.sh
+++ scripts/gcc-build/build-musl.sh
@@ -3,8 +3,8 @@
 
 BUILD_DIR=/docker/build
 CROSS_DIR=/opt/cross
-CONFIGF=/docker/scripts/gcc-build/config.mak
-MUSL_DIR=/docker/build/musl-cross-make
+CONFIGF="scripts/gcc-build/config.mak"
+MUSL_DIR="/docker/build/musl-cross-make"
 
 # The available target architectures
 # The currently set one is x84_64-linux-musl, use sed -i and change it in config.mak before copying it
@@ -15,18 +15,25 @@ MUSL_DIR=/docker/build/musl-cross-make
 # sh2eb="TARGET = sh2eb-linux-muslfdpic"
 printf '%s\n' "Setting architecture"
 sed -i 's/# ${x64}/${x64}/g' "$CONFIGF"
+
 printf '%s\n' "Creating $BUILD_DIR"
 mkdir -p "$BUILD_DIR"
+
 printf '%s\n' "Changing directory to $BUILD_DIR"
 cd "$BUILD_DIR"
+
 printf '%s\n' "Cloning musl-cross-make"
 git clone https://github.com/richfelker/musl-cross-make
+
 printf '%s\n' "Copying $CONFIGF to $BUILD_DIR/musl-cross-make"
 cp "$CONFIGF" "$MUSL_DIR"
+
 printf '%s\n' "Changing directory to $MUSL_DIR"
-cd "$MUSL_DIR"
+cd musl-cross-make
+
 printf '%s\n' "Changing isl mirror site to a responsive server"
-sed -i 's/ISL_SITE = http:\/\/isl.gforge.inria.fr\//ISL_SITE = https:\/\/gcc.gnu.org\/pub\/gcc\/infrastructure\//g' ./Makefile
+sed -i 's/ISL_SITE = http:\/\/isl.gforge.inria.fr\//ISL_SITE = https:\/\/gcc.gnu.org\/pub\/gcc\/infrastructure\//g' Makefile
+
 printf '%s\n' "Make musl cross-compiler"
 
 make
