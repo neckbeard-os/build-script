c6aef81fbfb1a6245f88c5d98b95873a42a1fce7
Victor-ray S <victorray91@pm.me>
2022-06-06 22:05:56 +0200

    Just added some details here and there
    Signed-off-by: Victor-ray S <victorray91@pm.me>

--- scripts/gcc-build/build-gcc.sh
+++ scripts/gcc-build/build-gcc.sh
@@ -10,40 +10,39 @@ colour() {
 		colour="$1"
 		reset="$(tput sgr0)"
 		printf '%s' "$colour$1$reset"
-	elif [ $# -eq 2 ] && [ "$1" -gt 0 ] && [ "$1" -lt 256 ]; then
+	elif [ $# -eq 2 ] && [ "${1}" -gt 0 ] && [ "$1" -lt 256 ]; then
 			reset="$(tput sgr0)"
-			c1="$(tput setaf $1)"
+			c1="$(tput setaf "${1}")"
 				printf '%s' "$c1$2$reset"
 				printf '\n';
 	elif [ $# -eq 4 ] && 
-			[ "$1" -gt 0 ] && [ "$1" -lt 256 ] &&
-			[ "$3" -gt 0 ] && [ "$3" -lt 256 ]; then
+			[ "${1}" -gt 0 ] && [ "${1}" -lt 256 ] &&
+			[ "${3}" -gt 0 ] && [ "${3}" -lt 256 ]; then
 			reset="$(tput sgr0)"
-			c1="$(tput setaf "$1")"
-			c2="$(tput setaf "$3")"
+			c1="$(tput setaf "${1}")"
+			c2="$(tput setaf "${3}")"
 				printf '%s ' "$c1$2$c2$4$reset"
 				printf '\n';
 	elif [ $# -eq 6 ] &&
-			[ "$1" -gt 0 ] && [ "$1" -lt 256 ] && 
-			[ "$3" -gt 0 ] && [ "$3" -lt 256 ] && 
-			[ "$5" -gt 0 ] && [ "$5" -lt 256 ]; then
+			[ "${1}" -gt 0 ] && [ "${1}" -lt 256 ] && 
+			[ "${3}" -gt 0 ] && [ "${3}" -lt 256 ] && 
+			[ "${5}" -gt 0 ] && [ "${5}" -lt 256 ]; then
 			reset="$(tput sgr0)"
-			c1="$(tput setaf $1)"
-			c2="$(tput setaf $3)"
-			c3="$(tput setaf $5)"
+			c1="$(tput setaf "${1}")"
+			c2="$(tput setaf "${3}")"
+			c3="$(tput setaf "${5}")"
 				printf '%s ' "$c1$2$c2$4$c3$6$reset"
 				printf '\n';
 	elif [ $# -eq 8 ] &&
-			[ "$1" -gt 0 ] && [ "$1" -lt 256 ] && 
-			[ "$3" -gt 0 ] && [ "$3" -lt 256 ] && 
-			[ "$5" -gt 0 ] && [ "$5" -lt 256 ] &&
-			[ "$7" -gt 0 ] && [ "$7" -lt 256 ]; then
+			[ "${1}" -gt 0 ] && [ "${1}" -lt 256 ] && 
+			[ "${3}" -gt 0 ] && [ "${3}" -lt 256 ] && 
+			[ "${5}" -gt 0 ] && [ "${5}" -lt 256 ] &&
+			[ "${7}" -gt 0 ] && [ "${7}" -lt 256 ]; then
 			reset="$(tput sgr0)"
-			c1="$(tput setaf "$1")"
-			c2="$(tput setaf "$3")"
-			c3="$(tput setaf "$5")"
-			c4="$(tput setaf "$7")"
-			c5="$(tput setaf "$9")"
+			c1="$(tput setaf "${1}")"
+			c2="$(tput setaf "${3}")"
+			c3="$(tput setaf "${5}")"
+			c4="$(tput setaf "${7}")"
 				printf '%s' "$c1$2$c2$4$c3$6$c4$8$reset"
 				printf '\n';
 	else
@@ -77,7 +76,7 @@ colour 46 "GCC-Build" 226 " $BUILD_GLIBC " 48 "Complete"
 
 colour 46 "GCC-Build" 15 ": Creating directories " 48 "Complete"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_DIR"
-cd "${BUILD_DIR}"
+cd "${BUILD_DIR}" || exit 1
 colour 46 "GCC-Build" 15 ": Downloading " 226 "binutils gcc kernel glibc mpfr gmp mpc isl cloog"
 wget https://mirrors.kernel.org/gnu/binutils/binutils-2.37.tar.xz
 wget https://mirrors.kernel.org/gnu/gcc/gcc-10.3.0/gcc-10.3.0.tar.xz
@@ -104,21 +103,21 @@ BINUTILS="${BUILD_DIR}/binutils-2.37"
 GCC="${BUILD_DIR}/gcc-10.3.0"
 KERNEL="${BUILD_DIR}/linux-5.15.12"
 GLIBC="${BUILD_DIR}/glibc-2.34"
-MPFR="${BUILD_DIR}/mpfr-4.1.0"
-GMP="BUILD_DIR}/gmp-6.2.0"
-MPC="$BUILD_DIR}/mpc-1.2.1"
-ISL="${BUILD_DIR}/isl-0.24"
-CLOOG="${BUILD_DIR}/cloog-0.18.1"
+# MPFR="${BUILD_DIR}/mpfr-4.1.0"
+# GMP="BUILD_DIR}/gmp-6.2.0"
+# MPC="$BUILD_DIR}/mpc-1.2.1"
+# ISL="${BUILD_DIR}/isl-0.24"
+# CLOOG="${BUILD_DIR}/cloog-0.18.1"
 ARCH="x86_x64"
 TARGET="amd64-linux"
-WMUSL=""
+# WMUSL=""
 HOST="amd64-linux"
 
 # Create symbolic links from the GCC directory to some of the other directories. 
 # These five packages are dependencies of GCC, and when the symbolic links are present, 
 # GCC’s build script will build them automatically.
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_DIR"
-cd "$GCC"
+cd "$GCC" || exit 1
 colour 46 "GCC-Build" 15 ": Creating symbolic links"
 colour 46 "GCC-Build" 15 ": Creating link " 226 "mpfr"
 ln -s ../mpfr-4.1.0 mpfr
@@ -132,7 +131,7 @@ colour 46 "GCC-Build" 15 ": Creating link " 226 "cloog"
 ln -s ../cloog-0.18.1 cloog
 colour 46 "GCC-Build" 15 ": Creating symbolic links " 48 "Complete"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_DIR"
-cd "$BUILD_DIR"
+cd "$BUILD_DIR" || exit 1
 
 # Choose an installation directory, and make sure you have write permission to it. 
 # In the steps that follow, I’ll install the new toolchain to /opt/cross.
@@ -150,40 +149,40 @@ colour 46 "GCC-Build" 15 ": Exporting /opt/cross/bin to PATH " 48 "Complete"
 # Build/install linux kernel headers
 colour 46 "GCC-Build" 15 ": Build/install linux kernel headers"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$KERNEL"
-cd "$KERNEL"
+cd "$KERNEL" || exit 1
 make ARCH="$ARCH" INSTALL_HDR_PATH="$CROSS_DIR/$TARGET" headers_install
 colour 46 "GCC-Build" 15 ": Build/install linux kernel headers" 48 "Complete"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_DIR"
-cd "$BUILD_DIR"
+cd "$BUILD_DIR" || exit 1
 
 # Build/install binutils
 colour 46 "GCC-Build" 15 ": Build/install binutils"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_DIR"
-cd "$BUILD_BINUTILS"
+cd "$BUILD_BINUTILS" || exit 1
 "$BINUTILS"/configure --prefix="$CROSS_DIR" --target="$TARGET" --disable-multilib
 make -j4
 make install
 colour 46 "GCC-Build" 15 ": Build/install binutils " 48 "Complete"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_DIR"
-cd "$BUILD_DIR"
+cd "$BUILD_DIR" || exit 1
 
 # Build GCC's C & C++ cross-compilers to /opt/cross/bin
 colour 46 "GCC-Build" 15 ": Build GCC's C & C++ cross-compilers to /opt/cross/bin"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_GCC"
-cd "$BUILD_GCC"
+cd "$BUILD_GCC" || exit 1
 "$GCC"/configure --prefix="$CROSS_DIR" --target="$TARGET" --enable-languages=c,c++ --disable-multilib
 make -j4 all-gcc
 make install-gcc
 colour 46 "GCC-Build" 15 ": Build GCC's C & C++ cross-compilers to /opt/cross/bin " 48 "Complete"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_DIR"
-cd "$BUILD_DIR"
+cd "$BUILD_DIR" || exit 1
 
 # Install glibc's standard C library headers to /opt/cross/amd64-linux/include
 colour 46 "GCC-Build" 15 ": Install glibc's standard C library to /opt/cross/amd64-linux"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_GLIBC"
-cd "$BUILD_GLIBC"
+cd "$BUILD_GLIBC" || exit 1
 # cd "$BUILD_MUSL"
-"$GLIBC"/configure --prefix="$CROSS_DIR/$TARGET" --build=$MACHTYPE --host="$HOST" --target="$TARGET" --with-headers="$CROSS_DIR/$TARGET/include" --disable-multilib libc_cv_forced_unwind=yes
+"$GLIBC"/configure --prefix="$CROSS_DIR/$TARGET" --build="$MACHTYPE" --host="$HOST" --target="$TARGET" --with-headers="$CROSS_DIR/$TARGET/include" --disable-multilib libc_cv_forced_unwind=yes
 make install-bootstrap-headers=yes install-headers
 make -j4 csu/subdir_lib
 install csu/crt1.o csu/crti.o csu/crtn.o "$CROSS_DIR/$TARGET/lib"
@@ -191,27 +190,27 @@ amd64-linux-gcc -nostdlib -nostartfiles -shared -x c /dev/null -o "$CROSS_DIR/$T
 touch "$CROSS_DIR/$TARGET/include/gnu/stubs.h"
 colour 46 "GCC-Build" 15 ": Install glibc's standard C library to /opt/cross/amd64-linux " 48 "Complete"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_DIR"
-cd "$BUILD_DIR"
+cd "$BUILD_DIR" || exit 1
 
 # Compiler support library
 colour 46 "GCC-Build" 15 ": Install compiler support library"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_GCC"
-cd "$BUILD_GCC"
+cd "$BUILD_GCC" || exit 1
 make -j4 all-target-libgcc
 make install-target-libgcc
 colour 46 "GCC-Build" 15 ": Install compiler support library " 48 "Complete"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_DIR"
-cd "$BUILD_DIR"
+cd "$BUILD_DIR" || exit 1
 
 # Standard C library
 colour 46 "GCC-Build" 15 ": Install GCC Standard C library"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_GCC"
-cd "$BUILD_GCC"
+cd "$BUILD_GCC" || exit 1
 make -j4
 make install
 colour 46 "GCC-Build" 15 ": Install GCC Standard C library " 48 "Complete"
 colour 46 "GCC-Build" 15 ": Changing directory to " 226 "$BUILD_DIR"
-cd "$BUILD_DIR"
+cd "$BUILD_DIR" || exit 1
 
 colour 46 "GCC-Build" 15 ": Build " 48 "Complete!"
 
--- scripts/gcc-build/build-musl.sh
+++ scripts/gcc-build/build-musl.sh
@@ -3,7 +3,7 @@
 # https://github.com/ZendaiOwl
 # 
 BUILD_DIR=/build
-CROSS_DIR=/opt/cross
+# CROSS_DIR=/opt/cross
 CONFIGF="scripts/gcc-build/config.mak"
 MUSL_DIR="$BUILD_DIR/musl-cross-make"
 
@@ -11,7 +11,7 @@ printf '%s\n' "Creating $BUILD_DIR"
 mkdir -p "$BUILD_DIR"
 
 printf '%s\n' "Changing directory to $BUILD_DIR"
-cd "$BUILD_DIR"
+cd "$BUILD_DIR" || exit 1
 
 printf '%s\n' "Cloning musl-cross-make"
 git clone https://github.com/richfelker/musl-cross-make
@@ -20,7 +20,7 @@ printf '%s\n' "Copying $CONFIGF to $BUILD_DIR/musl-cross-make"
 cp "/docker/scripts/gcc-build/config.mak" "$MUSL_DIR"
 
 printf '%s\n' "Changing directory to $MUSL_DIR"
-cd "$MUSL_DIR"
+cd "$MUSL_DIR" || exit 1
 
 printf '%s\n' "Changing isl mirror site to a responsive server"
 sed -i 's/ISL_SITE = http:\/\/isl.gforge.inria.fr\//ISL_SITE = https:\/\/gcc.gnu.org\/pub\/gcc\/infrastructure\//g' "./Makefile"
@@ -33,6 +33,7 @@ sed -i 's/ISL_SITE = http:\/\/isl.gforge.inria.fr\//ISL_SITE = https:\/\/gcc.gnu
 # armeabihf"TARGET = arm-linux-musleabihf"
 # sh2eb="TARGET = sh2eb-linux-muslfdpic"
 printf '%s\n' "Setting architecture"
+#shellcheck disable=SC2016
 sed -i 's/# ${x64}/${x64}/g' "./config.mak"
 
 printf '%s\n' "Make musl cross-compiler"
@@ -41,4 +42,4 @@ make
 
 printf '%s\n' "Complete!"
 
-exit
+exit 0
