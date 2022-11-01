71912355b87dfbbc6e45bbe134342cf8142cfdee
Owl <12261439+ZendaiOwl@users.noreply.github.com>
2022-11-01 13:51:10 +0100

    Update build-musl.sh

--- scripts/gcc-build/build-musl.sh
+++ scripts/gcc-build/build-musl.sh
@@ -10,12 +10,13 @@ debug() {
     printf '%s\n' "${PFX} ${*}"
 }
 # × × VARIABLES × × #
+GIT_REPO_MUSL="https://github.com/richfelker/musl-cross-make"
+TARGET="arm-linux-musleabi"
+OUTPUT="/opt/cross"
 CURRENT_DIR="$(pwd)"
 BUILD_DIR="${CURRENT_DIR}/build"
-CROSS_DIR="/opt/cross"
 CONFIGF="${CURRENT_DIR}/config.mak"
 MUSL_DIR="${BUILD_DIR}/musl-cross-make"
-GIT_REPO_MUSL="https://github.com/richfelker/musl-cross-make"
 # × × × × × × × × × × × × × × × × × × # 
 debug "Creating ${BUILD_DIR}"
 mkdir -p "${BUILD_DIR}"
@@ -36,16 +37,17 @@ debug "Changing ISL mirror site to a responsive server"
 sed -i 's|ISL_SITE = http://isl.gforge.inria.fr/|ISL_SITE = https://gcc.gnu.org/pub/gcc/infrastructure/|g' "./Makefile"
 # The available target architectures
 # The currently set one is x84_64-linux-musl, use sed -i and change it in config.mak before copying it
-# i486="TARGET = i486-linux-musl"
-# x64="TARGET = x86_64-linux-musl"
-# armeabi="TARGET = arm-linux-musleabi"
-# armeabihf"TARGET = arm-linux-musleabihf"
-# sh2eb="TARGET = sh2eb-linux-muslfdpic"
+# TARGET = i486-linux-musl
+# TARGET = x86_64-linux-musl
+# TARGET = arm-linux-musleabi
+# TARGET = arm-linux-musleabihf
+# TARGET = sh2eb-linux-muslfdpic
 # × × × × × × × × × × × × × × × × × × #
-debug "Setting architecture"
+debug "Setting target architecture to ${TARGET}"
 #shellcheck disable=SC2016
-sed -i 's/#TARGET = arm-linux-musleabi/TARGET = arm-linux-musleabi/' "./config.mak"
-# sed -i 's|#TARGET = x86_64-linux-musl|TARGET = x86_64-linux-musl|g' "./config.mak"
+sed -i '31a TARGET = '"${TARGET}"'' "./config.mak"
+debug "Setting output directory to ${OUTPUT}"
+sed -i '32a OUTPUT = '"${OUTPUT}"'' "./config.mak"
 # × × × × × × × × × × × × × × × × × × #
 debug "Make musl-cross-compiler"
 make
